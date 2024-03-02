093b0bce52335cd072a32e76a551a518adb74350
Gaurav <gauravkr7265@gmail.com>
2024-03-02 21:38:23 +0530

    file added

---
+++ 02.recursion/print_n_to_1_usingBacktracking.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int n){
+    if(i>n)
+    return;
+     f(i+1,n);
+    cout << i ;
+   
+
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number";
+    cin >> n;
+    f(1, n);
+}
